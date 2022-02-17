c7b63421eaf9ba6a34009a5281cdaa39fc27257f
Akane Sayama <a@azw.jp>
2022-02-17 17:26:20 +0900

    Add Blendshape2Animation

---
+++ Assets/AZW/BS2A/Editor/Blendshape2Animation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Blendshape2Animation : Editor
+{
+    const string DIR = "Assets/AZW/BS2A/GeneratedAnimations/";
+    [UnityEditor.MenuItem("Tools/AZW/Blendshape2Animation")]
+    public static void CreateAnim()
+    {
+        AssetDatabase.CreateFolder("Assets/AZW/BS2A", "GeneratedAnimations");
+
+        var go = Selection.activeGameObject;
+        var mesh = go.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        for (var i = 0; i <  mesh.blendShapeCount; i++)
+        {
+            var name = mesh.GetBlendShapeName(i);
+            AnimationClip clip = new AnimationClip();
+            AnimationUtility.SetEditorCurve(clip, EditorCurveBinding.FloatCurve(go.name, typeof(SkinnedMeshRenderer), "blendShape." + name), AnimationCurve.Linear(0, 0, 1, 1));
+            AssetDatabase.CreateAsset(clip, DIR + name + ".anim");
+        }
+    }
+}
---
+++ Assets/AZW/BS2A/README.md
@@ -0,0 +1,11 @@
+# Blendshape2Animation
+
+This program makes the animations (AimationClip) based on the blendshape.
+
+## Usage
+1. Select a mesh on the Hierarchy
+2. Click [Tools] > [AZW] > [Blendshape2Animation]
+3. The animation will be in `Assets/AZW/BS2A/GeneratedAnimations/`
+4. Move the generated files to your directory
+
+Note: this folder is only for temporary use. This script overwrites the file if there is a file with the same name in target directory
